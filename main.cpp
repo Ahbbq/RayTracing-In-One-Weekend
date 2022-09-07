418cdb67cf7f5e5b4b5a6ca6475d6c2016192c03
nanding <285037386@qq.com>
2022-09-07 19:12:28 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <iomanip>
+#include "ray.h"
+using namespace std;
+
+
+//判断光线是否与某个球相交
+double hit_sphere(const vec3& center, double radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	double a = dot(r.direction(), r.direction());
+	double b = 2.0 * dot(r.direction(), oc);
+	double c = dot(oc, oc) - radius * radius;
+	double discriminant = b * b - 4 * a * c;
+	if (discriminant < 0) {
+		return -1.0;
+	}
+	else {
+		double t1 = (-b - sqrt(discriminant)) / (2.0 * a);
+		if (t1 > 0)
+			return t1;
+		double t2 = (-b + sqrt(discriminant)) / (2.0 * a);
+		if (t2 > 0)
+			return t2;
+		return -1.0;
+	}
+}
+
+vec3 ray_color(const ray& r) {
+	double t = hit_sphere(vec3 (0, 0, -1), 0.5, r);
+	if (t > 0) {
+		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+		return 0.5 * vec3 (N.x() + 1, N.y() + 1, N.z() + 1);
+	}
+	vec3 unit_direction = unit_vector(r.direction());
+	t = 0.5 * (unit_direction.y() + 1.0);
+	//线性插值
+	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+
+int main() {
+    const int image_width = 200;
+    const int image_height = 100;
+
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: "  << setprecision(2) << fixed << ((float)(image_height - j ) / image_height) << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto u = double(i) / image_width;
+            auto v = double(j) / image_height;
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 color = ray_color(r);
+            color.write_color(std::cout);
+        }
+    }
+
+    std::cerr << "\nDone.\n";
+}
+		
+		
